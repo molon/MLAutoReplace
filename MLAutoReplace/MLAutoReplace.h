b989ade90872c7862399a4fa2bcc875ede966a46
molon <dudl@qq.com>
2014-04-25 21:09:46 +0800

    Initial Commit

---
+++ MLAutoReplace/MLAutoReplace.h
@@ -0,0 +1,13 @@
+//
+//  MLAutoReplace.h
+//  MLAutoReplace
+//
+//  Created by molon on 4/25/14.
+//  Copyright (c) 2014 molon. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface MLAutoReplace : NSObject
+
+@end
---
+++ MLAutoReplace/MLAutoReplace.m
@@ -0,0 +1,63 @@
+//
+//  MLAutoReplace.m
+//  MLAutoReplace
+//
+//  Created by molon on 4/25/14.
+//    Copyright (c) 2014 molon. All rights reserved.
+//
+
+#import "MLAutoReplace.h"
+
+static MLAutoReplace *sharedPlugin;
+
+@interface MLAutoReplace()
+
+@property (nonatomic, strong) NSBundle *bundle;
+@end
+
+@implementation MLAutoReplace
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource acccess
+        self.bundle = plugin;
+        
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
